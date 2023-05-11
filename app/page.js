fbfa1393180e9e41c5c781fb2bf8be6916151b83
BC <bc@Xubuntu.myguest.virtualbox.org>
2023-05-11 09:37:27 -0500

    Feat: added contact section

--- app/page.js
+++ app/page.js
@@ -1,13 +1,14 @@
 import Hero from '@/components/hero'
 import About from '@/components/about'
 import Projects from '@/components/projects'
-
+import Contact from '@/components/contact'
 const Page = () => {
   return (
     <>
     <Hero />
     <About />
     <Projects />
+    <Contact />
     </>
   );
 };
